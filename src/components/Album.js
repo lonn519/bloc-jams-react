bad96ce8230b437b2c9bd5815846742c6ba4552e
Lonn Lee <lonn.lee@gmail.com>
2018-10-01 00:15:57 -0700

    Checkpoint 09 Completed - Range

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,117 +12,148 @@ class Album extends Component {
 
         this.state = {
             album:album,
-            currentSong: '',
-            isPlaying: false,
-            currentHoveredSong: ''
+            currentSong: album.songs[0],
+            currentHoveredSong: '',
+            currentTime: 0,
+            duration: album.songs.duration,
+            isPlaying: false
         };
 
         this.audioElement = document.createElement('audio');
-        this.audioElement.src = '';
+        this.audioElement.src = album.songs[0].audioSrc;
     }
-        play() {
-            this.audioElement.play();
-            this.setState({isPlaying:true});
-        }
+
+    componentDidMount(){
+        this.eventListeners = {
+            timeupdate: e => {
+                this.setState({currentTime:this.audioElement.currentTime});
+            },
+            durationchange: e=> {
+                this.setState({duration: this.audioElement.duration})
+            }
+        };
+        this.audioElement.addEventListener('timeupdate',this.eventListeners.timeupdate);
+        this.audioElement.addEventListener('durationchange',this.eventListeners.durationchange);
+    }
+
+    componentWillUnmount(){
+        this.audioElement.src=null;
+        this.audioElement.removeEventListener('timeupdate',this.eventListeners.timeupdate);
+        this.audioElement.removeEventListener('durationchange',this.eventListeners.durationchange);
+    }
+
+    play() {
+        this.audioElement.play();
+        this.setState({isPlaying:true});
+    }
         
-        pause() {
-            this.audioElement.pause();
-            this.setState({isPlaying:false});
-        }
-
-        setSong(song) {
-            this.audioElement.src = song.audioSrc;
-            this.setState ({currentSong:song});
-        }
-
-        handleSongClick(song){
-            const isSameSong = this.state.currentSong === song;
-            if (this.state.isPlaying && isSameSong){
-                this.pause();
-            }else{
-                if (!isSameSong) { this.setSong(song);}
-                this.play();
-            }
-        }
-
-        manageSongIcon(song,index){
-            const isSameSong = this.state.currentSong === song;
-            if (this.state.isPlaying && isSameSong){
-                return <span className='ion-ios-pause'></span>;
-            }else if (!this.state.isPlaying && isSameSong){
-                return <span className='ion-ios-play'></span>;
-            } else {
-                if (this.state.currentHoveredSong===index) {
-                    return <span className='ion-ios-play'></span>;
-                }else{
-                    return index+1;
-                }
-            }
-        }
-
-        handleSongHover(index){
-            this.setState({currentHoveredSong:index});
-        }
-
-        handleSongLeave(index){
-            this.setState({currentHoveredSong:''})
-        }
-
-        handlePreviousClick(){
-            const currentIndex = this.state.album.songs.findIndex(song=>this.state.currentSong===song);
-            const newIndex = Math.max(0,currentIndex-1);
-            const newSong = this.state.album.songs[newIndex];
-            this.setSong(newSong);
-            this.play();
-        }
-
-        handleNextClick(){
-            const currentIndex = this.state.album.songs.findIndex(song=>this.state.currentSong===song);
-            const newIndex = Math.min(this.state.album.songs.length-1,currentIndex+1);
-            const newSong = this.state.album.songs[newIndex];
-            this.setSong(newSong);
-            this.play();
-        }
-
-
-    render(){
-        return(
-            <section className='album'>
-                <section id='album-info'>
-                    <img id='album-cover-art' src={this.state.album.albumCover} alt='{this.state.album.title}' />
-                    <div className='album-details'>
-                        <h1 id='album-title'>{this.state.album.title}</h1>
-                        <h2 className='artist'>{this.state.album.artist}</h2>
-                        <div id='release-info'>{this.state.album.releaseInfo}</div>
-                    </div>
-                </section>
-                <table id='song-list'>
-                    <colgroup>
-                        <col id='song-number-column' />
-                        <col id='song-title-column' />
-                        <col id='song-duration-column' />
-                    </colgroup>
-                    <tbody>
-                        {
-                            this.state.album.songs.map((song,index)=>
-                            <tr className='song' key={index} onClick={()=>this.handleSongClick(song)} onMouseEnter={()=>this.handleSongHover(index)} onMouseLeave={()=>this.handleSongLeave(index)}>
-                                <td>{this.manageSongIcon(song,index)}</td>
-                                <td>{song.title}</td>
-                                <td>{song.duration}</td>
-                            </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-                <PlayerBar 
-                    isPlaying={this.state.isPlaying} 
-                    currentSong={this.state.currentSong}
-                    handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-                    handlePreviousClick={()=> this.handlePreviousClick()}
-                    handleNextClick={()=> this.handleNextClick()}/>
-            </section>
-        );
+    pause() {
+        this.audioElement.pause();
+        this.setState({isPlaying:false});
     }
+
+    setSong(song) {
+        this.audioElement.src = song.audioSrc;
+        this.setState ({currentSong:song});
+    }
+
+    handleSongClick(song){
+        const isSameSong = this.state.currentSong === song;
+        if (this.state.isPlaying && isSameSong){
+            this.pause();
+        }else{
+            if (!isSameSong) { this.setSong(song);}
+            this.play();
+        }
+    }
+
+    handleTimeChange(e){
+        const newTime = this.audioElement.duration *e.target.value;
+        this.audioElement.currentTime = newTime;
+        this.setState({currentTime:newTime});
+    }
+
+    manageSongIcon(song,index){
+        const isSameSong = this.state.currentSong === song;
+        if (this.state.isPlaying && isSameSong){
+            return <span className='ion-ios-pause'></span>;
+        }else if (!this.state.isPlaying && isSameSong){
+            return <span className='ion-ios-play'></span>;
+        } else {
+            if (this.state.currentHoveredSong===index) {
+                return <span className='ion-ios-play'></span>;
+            }else{
+                return index+1;
+            }
+        }
+    }
+
+    handleSongHover(index){
+        this.setState({currentHoveredSong:index});
+    }
+
+    handleSongLeave(index){
+        this.setState({currentHoveredSong:''})
+    }
+
+    handlePreviousClick(){
+        const currentIndex = this.state.album.songs.findIndex(song=>this.state.currentSong===song);
+        const newIndex = Math.max(0,currentIndex-1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
+
+    handleNextClick(){
+        const currentIndex = this.state.album.songs.findIndex(song=>this.state.currentSong===song);
+        const newIndex = Math.min(this.state.album.songs.length-1,currentIndex+1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
+
+
+render(){
+    return(
+        <section className='album'>
+            <section id='album-info'>
+                <img id='album-cover-art' src={this.state.album.albumCover} alt='{this.state.album.title}' />
+                <div className='album-details'>
+                    <h1 id='album-title'>{this.state.album.title}</h1>
+                    <h2 className='artist'>{this.state.album.artist}</h2>
+                    <div id='release-info'>{this.state.album.releaseInfo}</div>
+                </div>
+            </section>
+            <table id='song-list'>
+                <colgroup>
+                    <col id='song-number-column' />
+                    <col id='song-title-column' />
+                    <col id='song-duration-column' />
+                </colgroup>
+                <tbody>
+                    {
+                        this.state.album.songs.map((song,index)=>
+                        <tr className='song' key={index} onClick={()=>this.handleSongClick(song)} onMouseEnter={()=>this.handleSongHover(index)} onMouseLeave={()=>this.handleSongLeave(index)}>
+                            <td>{this.manageSongIcon(song,index)}</td>
+                            <td>{song.title}</td>
+                            <td>{song.duration}</td>
+                        </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+            <PlayerBar 
+                isPlaying={this.state.isPlaying} 
+                currentSong={this.state.currentSong}
+                currentTime={this.audioElement.currentTime}
+                duration={this.audioElement.duration}
+                handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+                handlePreviousClick={()=> this.handlePreviousClick()}
+                handleNextClick={()=> this.handleNextClick()}
+                handleTimeChange={(e)=> this.handleTimeChange(e)}/>
+        </section>
+    );
+}
 }
 
 export default Album;
